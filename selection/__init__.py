2d011231e0277f2f4936223dfd8472dd0c96d7a0
Aravind Rajesh Kanna <L00163466@student.lyit.ie>
2021-09-27 10:12:37 +0100

    First commit

---
+++ selection/__init__.py
@@ -0,0 +1,13 @@
+"""
+#
+# File        : __init__.py.py
+# Created     : 27/09/21 9:54 AM
+# Author      : R. Aravind
+# Version     : v1.0.0
+# Licencing   : (C) 2021 Aravind Rajesh Kanna, LYIT
+                Available under GNU Public License (GPL)
+# Creadits    :
+# Maintainer  :
+# Description : 
+#
+"""
---
+++ selection/sample_if.py
@@ -0,0 +1,14 @@
+"""
+#
+# File        : sample_if.py
+# Created     : 27/09/21 10:08 AM
+# Author      : R. Aravind
+# Version     : v1.0.0
+# Licencing   : (C) 2021 Aravind Rajesh Kanna, LYIT
+                Available under GNU Public License (GPL)
+# Creadits    :
+# Maintainer  :
+# Description : If example
+#
+"""
+
