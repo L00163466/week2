c72383c99a9016f2e86d35ce96df51e40f77c735
Aravind Rajesh Kanna <L00163466@student.lyit.ie>
2021-09-27 10:30:32 +0100

    Selection sample

--- selection/sample_if.py
+++ selection/sample_if.py
@@ -12,3 +12,20 @@
 #
 """
 
+
+if __name__ == '__main__':
+    grade = input("Enter a grade: ")
+    grade = int(grade)
+
+    module_1 = "python"
+
+    if grade >= 70 and module_1 == "python":
+        print("You have earned a Distinction!")
+    elif grade >= 60:
+        print("You have earned a M.1.!")
+    elif grade >= 50:
+        print("You have earned a M.2.!")
+    elif grade >= 40:
+        print("You passed!")
+    else:
+        print("Please try again")
